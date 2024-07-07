b0ffa035b062786ae4d83086662a425276d0bdfc
Aditya Shrivastav <aditya.shri9981@gmail.com>
2024-07-07 09:58:00 +0530

    login route completed

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -23,3 +23,19 @@ def login():
     auth = request.authorization
     if not auth:
         return {"message": "Missing username or password"}, 401
+
+    cur = mysql.connection.cursor()
+    res = cur.execute(
+        "SELECT email, password FROM users WHERE email = %s", (auth.username,)
+    )
+
+    if res == 0:
+        return {"message": "Invalid credentials"}, 404
+
+    user_row = cur.fetchone()
+    password = user_row[1]
+
+    if auth.password != password:
+        return {"message": "Invalid password"}, 401
+    else:
+        return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
