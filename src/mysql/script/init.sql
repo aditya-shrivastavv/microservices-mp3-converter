2a578e3a7b59b82fd4423862271edea1740a2571
Aditya Shrivastav <aditya.shri9981@gmail.com>
2024-07-13 14:14:44 +0530

    mysql credn corrections

--- src/mysql/script/init.sql
+++
@@ -1,9 +0,0 @@
-USE auth;
-
-CREATE TABLE user (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
-
-INSERT INTO user (email, password) VALUES ("aditya@email.com", "admin123");
